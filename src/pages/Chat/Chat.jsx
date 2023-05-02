cc1c1114f6ab1c03a2e201a7403d18c0089f596e
Alen Zgurć <zguric.corp@gmail.com>
2023-05-02 23:42:45 +0200

    add favicon

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,6 +1,7 @@
 import CookiePopup from "../../Components/CookiePopup/js/CookiePopup";
 import PageTitle from "../../Components/Header/js/PageTitle";
 
+
 export default function Chat(){
     return(
           <main>
@@ -8,13 +9,7 @@ export default function Chat(){
              <CookiePopup/>
 
             <section>
-            <div className="title">
-            <h1>FRONT-END DEVELOPER</h1>
-            <h3>završni rad</h3>
-            </div>
-            <div className="files">
-                <h4>App Dostupan na:</h4>
-            </div>        
+            
             </section>
         </main> 
     );
