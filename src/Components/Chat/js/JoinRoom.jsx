0f434e0da3f82072df143a583bed63be53a30cbe
Alen Zgurć <zguric.corp@gmail.com>
2023-05-07 05:00:59 +0200

    add handleKeyPress in JoinRoom

--- src/Components/Chat/js/JoinRoom.jsx
+++ src/Components/Chat/js/JoinRoom.jsx
@@ -12,10 +12,16 @@ export default function JoinRoom(props) {
     }
   }
 
+  function handleKeyPress(event) {
+    if (event.key === "Enter") {
+      handleSend();
+    }
+  }
+
   return (
     <div className="insertChatname">
       <h2>Unesi Ime </h2>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <input value={name} onChange={(e) => setName(e.target.value)} onKeyPress={handleKeyPress} />
       <button onClick={handleSend}>Prijavi se</button>
     </div>
   );
